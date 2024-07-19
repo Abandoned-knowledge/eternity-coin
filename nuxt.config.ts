ff940dc18c6f5350d7b85be04281c275b9479426
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-19 11:41:24 +0300

    npm sass, create scss/

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  css: ["./assets/scss/main.scss"],
+  
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 });
