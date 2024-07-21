da40c3af4f99b9c7680b63c9eb412790f708f1e6
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-21 15:59:19 +0300

    create CurrentValue.vue

---
+++ src/components/CurrentValue.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="frame frame_chart flex flex-col gap-5">
+    <p class="title-text text-grey">Current Value</p>
+    <p class="title-text">123123 ₽</p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,11 @@
   <div class="flex gap-5">
     <ChartsExpenseDonutFirst class="w-[30%]" />
     <ChartsIncomeDonutFirst class="w-[30%]" />
+
+    <div class="flex flex-grow flex-col gap-5">
+      <AddTransaction />
+      <CurrentValue class="flex-grow" />
+    </div>
   </div>
 </template>
 
