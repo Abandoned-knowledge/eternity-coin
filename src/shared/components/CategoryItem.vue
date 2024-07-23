675410a0d781f9a4a1bae3da8be9f5879887a5fe
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-23 14:31:48 +0300

    create CategoryItem.vue and TagItem.vue

---
+++ src/shared/components/CategoryItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { type ICategoryItem } from "~/app/interfaces/interfaces";
+const props = defineProps<ICategoryItem>();
+</script>
+
+<template>
+  <div class="body-text w-fit rounded-[10px] px-3 py-1 text-white" :style="{ backgroundColor: props.color }">
+    {{ props.title }}
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/shared/components/TagItem.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="body-text w-fit rounded-[10px] bg-grey px-3 py-1 text-white">
+    <slot></slot>
+  </div>
+</template>
