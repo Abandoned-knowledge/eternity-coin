f3a356bcc3d5baabaf1612053e5879cc21d0b762
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-23 14:32:50 +0300

    create Summary line chart

---
+++ src/components/Charts/Summary/LineFirst/Chart.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { LineChartOptions } from "@/components/Charts/options";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Line } from "vue-chartjs";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+const data = {
+  labels: ["June", "July", "August"],
+  datasets: [
+    {
+      label: "Income",
+      backgroundColor: "#519872",
+      borderColor: "#519872",
+      data: [23, 54, 12],
+      tension: 0.3,
+    },
+    {
+      label: "Expense",
+      backgroundColor: "#E94F37",
+      borderColor: "#E94F37",
+      data: [44, 11, 15],
+      tension: 0.3,
+    },
+  ],
+};
+</script>
+
+<template>
+  <Line :data="data" :options="LineChartOptions" />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Charts/Summary/LineFirst/Index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Chart from "./Chart.vue";
+</script>
+
+<template>
+  <div class="frame frame_chart mt-10 h-[40vh]">
+    <Chart />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
