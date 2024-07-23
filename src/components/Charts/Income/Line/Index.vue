25fdd2fe295deba14d119cb3f9695c79acbd0b02
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-23 14:33:04 +0300

    create income line chart

---
+++ src/components/Charts/Income/Line/Index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { LineChartOptions } from "~/components/Charts/options";
+import { Line } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "Data One",
+      backgroundColor: "#f87979",
+      borderColor: "#f87979",
+      data: [40, 39, 10, 40, 39, 80, 40],
+    },
+  ],
+};
+</script>
+
+<template>
+  <Line :options="LineChartOptions" :data="data" />
+</template>
+
+<style lang="scss" scoped></style>
