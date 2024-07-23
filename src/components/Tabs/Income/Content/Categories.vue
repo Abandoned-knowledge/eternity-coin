eac64cd4a3609f4788770a2d2fb2245b40e21384
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-23 14:43:04 +0300

    add forms to create category and tag in income tabs/categories

--- src/components/Tabs/Income/Content/Categories.vue
+++ src/components/Tabs/Income/Content/Categories.vue
@@ -22,8 +22,21 @@ const { data: tags } = await useFetch("/api/income/tags");
       </div>
     </div>
 
-    <form action="/" method="post" class="frame frame_chart">
-      <Input :outline-color="'income'" placeholder="Category Name"> Category Name </Input>
+    <form action="/" method="post" class="frame frame_chart flex flex-col gap-5">
+      <p class="title-text">Create Category</p>
+      <Input required :outline-color="'income'" placeholder="Category Name"> Category Name </Input>
+
+      <input type="color" id="button-select-color" hidden />
+      <label for="button-select-color" class="button button_accent body-text">Select Color</label>
+      <button type="submit" class="button button_main body-text">Add</button>
+    </form>
+
+    <form action="/" method="post" class="frame frame_chart flex flex-col gap-5 h-fit">
+      <p class="title-text">Create Tag</p>
+      <Input required :outline-color="'income'" placeholder="Tag Name"> Tag Name </Input>
+
+      <input type="color" id="button-select-color" hidden />
+      <button type="submit" class="button button_main body-text">Add</button>
     </form>
   </div>
 </template>
