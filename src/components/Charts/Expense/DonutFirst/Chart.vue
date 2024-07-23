6f360b96c96430a77ae7fdda90447877a8d8a66e
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-23 14:32:23 +0300

    remove options from charts DonutFirst

--- src/components/Charts/Expense/DonutFirst/Chart.vue
+++ src/components/Charts/Expense/DonutFirst/Chart.vue
@@ -1,19 +1,14 @@
 <template>
-  <Doughnut :options="chartOptions" :data="chartData" />
+  <Doughnut :options="doughnutChartOptions" :data="chartData" />
 </template>
 
 <script lang="ts" setup>
 import { Doughnut } from "vue-chartjs";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import { Chart as ChartJS, Tooltip, Legend, ArcElement, layouts } from "chart.js";
+import { doughnutChartOptions } from "~/components/Charts/options";
 
 ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
-
-// const style = getComputedStyle(document.body);
-// const whiteColor = style.getPropertyValue("--bg-color");
-// const darkColor = style.getPropertyValue("--dark-color");
-const whiteColor = "#ffffff";
-const darkColor = "#000000";
 
 const chartData = reactive({
   labels: ["Groceries", "Auto", "Metro"],
@@ -25,27 +20,4 @@ const chartData = reactive({
     },
   ],
 });
-
-const chartOptions = {
-  plugins: {
-    legend: {
-      position: "bottom",
-      align: "center",
-      labels: {
-        color: darkColor,
-        font: {
-          size: 14,
-          family: "MPLUS",
-        },
-      },
-    },
-    datalabels: {
-      color: whiteColor,
-      font: {
-        family: "MPLUS",
-        size: 18,
-      },
-    },
-  },
-};
 </script>
