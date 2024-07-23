28e00e8cd9450a83ccfce64196a22acc38f96685
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-23 14:31:06 +0300

    create Input.vue

---
+++ src/shared/components/Input.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{ placeholder: string; outlineColor: string }>();
+
+function convertPlaceholder(ph: string): string {
+  const words = ph.split(" ");
+  const wordsLowerCase = words.map((word) => word.toLowerCase());
+  return `input-${wordsLowerCase.join("-")}`;
+}
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <label :for="convertPlaceholder(props.placeholder)" class="body-text body-text_sm ml-1 text-grey">
+      <slot></slot>
+    </label>
+    <input
+      type="text"
+      :id="convertPlaceholder(props.placeholder)"
+      class="body-text rounded border border-main p-1 outline-none outline-2 outline-offset-0"
+      :class="`focus:outline-${props.outlineColor}`"
+      :placeholder="props.placeholder"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
