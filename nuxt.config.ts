3e9d5b62dbee3d754d5a5895f59d310276bd9d5f
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-24 19:31:46 +0300

    add dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  
+  modules: ["@prisma/nuxt"],
 
   ssr: true,
-
   srcDir: "./src",
 
   dir: {
-    layouts: "./app/layouts/",
+    layouts: "~/app/layouts/",
   },
 
   css: ["~/app/scss/tailwind.scss", "~/app/scss/main.scss"],
