8b001fe080e1c3c9aaa545856eb52c4392706f78
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-25 16:49:08 +0300

    add nitro.esbuild.options.target = 'esnext' to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
 
+  nitro: {
+    esbuild: {
+      options: {
+        target: "esnext",
+      },
+    },
+  },
+
   devtools: { enabled: true },
 
   modules: ["@prisma/nuxt"],
 
   prisma: {
     runMigration: false,
-    installStudio: false
+    installStudio: false,
   },
 
   ssr: false,
