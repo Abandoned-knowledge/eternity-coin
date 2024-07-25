3745711f766668dc9ed0c12e70925dea077d6ad4
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-25 16:50:19 +0300

    create entity user

---
+++ entities/user/index.ts
@@ -0,0 +1,11 @@
+import { type IUser } from "~/app/interfaces/interfaces";
+
+
+const user: IUser = {
+  user_id: 1,
+  email: "test@email.com",
+  password: "123123",
+  name: "first last",
+};
+
+export default user;
