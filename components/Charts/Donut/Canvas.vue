c3b3df6167fe643baaea15eafac843f0f387d073
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-26 19:10:01 +0300

    move data from canvas to index in ChartsDonut

--- components/Charts/Donut/Canvas.vue
+++ components/Charts/Donut/Canvas.vue
@@ -1,39 +1,15 @@
 <template>
-  <Doughnut ref="donut_chart" :options="doughnutChartOptions" :data="chartData" />
+  <Doughnut :options="doughnutChartOptions" :data="props.data" />
 </template>
 
 <script lang="ts" setup>
 import { Doughnut } from "vue-chartjs";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 import { Chart, Tooltip, Legend, ArcElement, layouts } from "chart.js";
+
 Chart.register(ArcElement, Tooltip, Legend, ChartDataLabels);
+import { doughnutChartOptions } from "~/features/charts/options";
 
 import { type IDonutItem } from "~/app/interfaces/interfaces";
-import { doughnutChartOptions } from "~/features/charts/options";
-const props = defineProps<{ data: IDonutItem[] }>();
-
-const chartData = computed(() => {
-  return {
-    labels: props.data.map((el) => el.label),
-    datasets: [
-      {
-        backgroundColor: props.data.map((el) => el.color),
-        data: props.data.map((el) => Number(el.total_value)),
-        hoverOffset: 15,
-      },
-    ],
-  };
-});
-
-const donut_chart = ref(null);
-
-watch(
-  () => props.data,
-  () => {
-    chartData.value.labels = props.data.map((el) => el.label);
-    chartData.value.datasets[0].data = props.data.map((el) => Number(el.total_value));
-    chartData.value.datasets[0].backgroundColor = props.data.map((el) => el.color);
-  },
-  { immediate: true },
-);
+const props = defineProps();
 </script>
--- components/Charts/Donut/Index.vue
+++ components/Charts/Donut/Index.vue
@@ -11,10 +11,31 @@ const date = ref<dateType>("month");
 const { data } = await useFetch(`/api/transactions/${type.value}/date/${date.value}`);
 let rawData = ref<IDonutItem[]>(data.value as IDonutItem[]);
 
-watch(date, async () => {
-  const data = await $fetch(`/api/transactions/${type.value}/date/${date.value}`);
-  rawData.value = data as IDonutItem[];  
-})
+const chartData = computed(() => {
+  return {
+    labels: rawData.value.map((el) => el.label),
+    datasets: [
+      {
+        backgroundColor: rawData.value.map((el) => el.color),
+        data: rawData.value.map((el) => Number(el.total_value)),
+        hoverOffset: 15,
+      },
+    ],
+  };
+});
+
+watch(
+  date,
+  async () => {
+    const data = await $fetch(`/api/transactions/${type.value}/date/${date.value}`);
+    rawData.value = data as IDonutItem[];
+
+    chartData.value.labels = rawData.value.map((el) => el.label);
+    chartData.value.datasets[0].data = rawData.value.map((el) => Number(el.total_value));
+    chartData.value.datasets[0].backgroundColor = rawData.value.map((el) => el.color);
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
@@ -22,9 +43,9 @@ watch(date, async () => {
     <div class="flex w-full items-center justify-between">
       <p class="title-text capitalize">{{ props.type }}</p>
 
-      <ChartSelect @test-name="(e) => (date = e)" />
+      <ChartSelect @test-name="(type) => (date = type)" />
     </div>
 
-    <Canvas :data="rawData" />
+    <Canvas :data="chartData" />
   </article>
 </template>
