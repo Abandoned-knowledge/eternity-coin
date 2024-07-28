1aef4c51d3139603ac56e4b2548f5eb734f45663
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-28 12:42:35 +0300

    create adding TransactionItem.vue to Recent

--- components/Recent/Index.vue
+++ components/Recent/Index.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import TransactionItem from "./TransactionItem.vue";
+import type { ITransactionItem } from "~/app/interfaces/interfaces";
+const { data } = await useFetch<ITransactionItem[]>("/api/transactions/all");
 </script>
 
 <template>
   <aside class="frame frame_sidebar min-w-fit overflow-y-scroll px-5 pb-5">
     <p class="title-text">Recent transactions</p>
 
-    <ul class="mt-10 flex flex-col gap-5">
-      <TransactionItem :is-income="true" />
-      <TransactionItem :is-income="false" />
+    <ul class="mt-10 flex flex-col gap-8">
+      <TransactionItem
+        v-for="item in data"
+        :date="new Date(item.date)"
+        :is-income="item.transaction_type_id == 1"
+        :label="item.label"
+        :value="item.value"
+      />
     </ul>
   </aside>
 </template>
