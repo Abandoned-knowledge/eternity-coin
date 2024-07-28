3c60d1c86bed8cd52f215b7741c3a0c301f4dee8
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-28 17:26:44 +0300

    create Tabs/Income/Transactions.vue

---
+++ components/Tabs/Income/Transactions.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import type { ITransactionItem } from "~/app/interfaces/interfaces";
+const { data: incomeData } = await useFetch<ITransactionItem[]>("/api/transactions/income/date/all");
+</script>
+
+<template>
+  <TableTransactions :data="incomeData!" />
+</template>
