afcfcf1758f1cf737fda5c918646953af1455444
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-29 16:32:52 +0300

    add orderBy to api/transactions/[type]

--- server/api/transactions/[type]/index.ts
+++ server/api/transactions/[type]/index.ts
@@ -6,27 +6,28 @@ export default defineEventHandler(async (event) => {
   const { type } = event.context.params!;
   const transactionType = type == "income" ? 1 : 2;
   let transactions;
-  
-  const select = {
-    value: true,
-    date: true,
-    categories: {
-      select: {
-        category_id: true,
-        color: true,
-        label: true,
-        type_id: true,
-      },
-    },
-  };
 
   transactions = await prisma.transactions.findMany({
-    select: select,
+    select: {
+      value: true,
+      date: true,
+      categories: {
+        select: {
+          category_id: true,
+          color: true,
+          label: true,
+          type_id: true,
+        },
+      },
+    },
     where: {
       user_id: user.user_id,
       categories: {
         type_id: transactionType,
       },
+    },
+    orderBy: {
+      date: "desc",
     },
   });
 
