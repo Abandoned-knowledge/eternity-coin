1ef65f99832866770037fe2c854d4d641b6c8ea5
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-07-30 03:03:22 +0300

    fix: create Forms/Login

---
+++ components/Forms/Login.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import type { IUser } from "~/app/interfaces/interfaces";
+import { Authorization } from "~/features/account/login";
+
+const warningMessage = ref<string | null>(null);
+
+async function submitForm(event: Event) {
+  const form = event.target as HTMLFormElement;
+
+  const login = (form.querySelector('input[name="input_enter_the_email"]') as HTMLInputElement).value;
+  const password = (form.querySelector('input[name="input_enter_the_password"]') as HTMLInputElement).value;
+
+  if (!login) warningMessage.value = "Please, enter the login";
+  else if (!password) warningMessage.value = "Please, enter the password";
+
+  const user: IUser[] | string = await $fetch("/api/account/login", {
+    method: "get",
+    body: {
+      login: login,
+      password: password,
+    },
+  });
+
+  if (typeof user == "string") {
+    warningMessage.value = user;
+  } else {
+    warningMessage.value = null;
+    Authorization(user[0]);
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="submitForm($event)" class="frame frame_chart flex h-fit w-[50%] flex-col items-center gap-5">
+    <p class="title-text">Login</p>
+
+    <Input type="email" label="Email" placeholder="Enter the email" :required="true"  />
+    <Input type="password" label="password" placeholder="Enter the password" :required="true" />
+
+    <p v-show="warningMessage" class="body-text body-text_lg text-red">{{ warningMessage }}</p>
+    <button type="submit" class="button button_main">Sign In</button>
+    <p class="body-text flex flex-col items-center gap-2">
+      Do not you have an account?
+      <a href="/register" class="uppercase text-link underline">create</a>
+    </p>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
